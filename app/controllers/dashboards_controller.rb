112647a13659c0c0ae12de32d21d4ddaa26bbb9f
Suman Ghosh <sghosh968@gmail.com>
2015-11-11 14:58:27 +0530

    Devise initial setup

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,19 @@
+class DashboardController < ApplicationController
+
+  def homepage
+
+
+  end
+
+
+
+  def user_dashboard
+
+
+
+  end
+
+
+  private
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :commments
   resources :posts
+
+  root_to: 'dashboard#homepage'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
